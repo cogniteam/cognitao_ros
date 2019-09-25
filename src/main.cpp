198f962112f0d1553d910b9952cddc739375870e
lin <linazan23@gmail.com>
2019-09-25 19:03:03 +0300

    updated node ros

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,25 @@
 #include <stdlib.h>
 #include <iostream>
 #include <CogniTAO.h>
+#include "ros/ros.h"
 
-int main(){
-    std::cout<<"node is running"<<std::endl;
-    config("/home/lin/dm_ws/src/cognitao.git/config_lin.json");
+int main(int argc, char **argv) {
+
+    ros::init(argc, argv, "some_node");
+
+    std::cout<<"node running"<<std::endl;
+
+    // cerr << " getenv(home) " << getenv("HOME") << endl;
+    // cerr << " path " << getenv("HOME") << "/dm_ros/src/" << endl;
+
+    stringstream ss;
+
+    ss << getenv("HOME") << "/dm_ros/src/" << "cognitao.git/config_lin.json";
+
+    cerr << ss.str() << endl;
+
+
+    config(ss.str()); 
    
     return 0;
 }
