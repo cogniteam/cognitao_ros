5fafbd715f1fbf91ff99dc4a0b21d04e013b7346
Lin <lin@cogniteam.com>
2019-09-25 16:29:43 +0300

    initial

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <CogniTAO.h>
+
+int main(){
+    std::cout<<"node is running"<<std::endl;
+    config("/home/lin/dm_ws/src/cognitao.git/config_lin.json");
+   
+    return 0;
+}
