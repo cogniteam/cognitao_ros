103436542d6de783b7e2e31d447be56ec3232c37
lin <linazan23@gmail.com>
2020-03-19 17:52:06 +0200

    wip-refactoring runner+rosdatasource

--- src/client/Ros1Runner.cpp
+++ src/client/Ros1Runner.cpp
@@ -1,44 +1,94 @@
+/*
+ * Ros1Runner.cpp
+ * 
+ * @author Lin Azan (lin@cogniteam.com)
+ * @date 2020-03-15
+ * @copyright Cogniteam (c) 2020
+ *
+ *
+ * Cogniteam LTD CONFIDENTIAL
+ *
+ * Unpublished Copyright (c) 2010-2020 Cogniteam,        All Rights Reserved.
+ *
+ * NOTICE:  All information contained  herein  is,  and  remains the property
+ * of Cogniteam.   The   intellectual   and   technical   concepts  contained
+ * herein are proprietary to Cogniteam and may  be  covered  by  Israeli  and
+ * Foreign Patents, patents in process,  and  are  protected  by trade secret
+ * or copyright law. Dissemination of  this  information  or  reproduction of
+ * this material is strictly forbidden unless  prior  written  permission  is
+ * obtained  from  Cogniteam.  Access  to  the  source  code contained herein
+ * is hereby   forbidden   to   anyone  except  current  Cogniteam employees,
+ * managers   or   contractors   who   have   executed   Confidentiality  and
+ * Non-disclosure    agreements    explicitly    covering     such     access
+ *
+ * The copyright notice  above  does  not  evidence  any  actual  or intended
+ * publication  or  disclosure    of    this  source  code,   which  includes
+ * information that is confidential  and/or  proprietary,  and  is  a   trade
+ * secret, of   Cogniteam.    ANY REPRODUCTION,  MODIFICATION,  DISTRIBUTION,
+ * PUBLIC   PERFORMANCE,  OR  PUBLIC  DISPLAY  OF  OR  THROUGH USE   OF  THIS
+ * SOURCE  CODE   WITHOUT   THE  EXPRESS  WRITTEN  CONSENT  OF  Cogniteam  IS
+ * STRICTLY PROHIBITED, AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL
+ * TREATIES.  THE RECEIPT OR POSSESSION OF  THIS SOURCE  CODE  AND/OR RELATED
+ * INFORMATION DOES  NOT CONVEY OR IMPLY ANY RIGHTS  TO  REPRODUCE,  DISCLOSE
+ * OR  DISTRIBUTE ITS CONTENTS, OR TO  MANUFACTURE,  USE,  OR  SELL  ANYTHING
+ * THAT      IT     MAY     DESCRIBE,     IN     WHOLE     OR     IN     PART
+ *
+ */
+
+
 #include <cognitao_ros/client/Ros1Runner.h>
 
-// void h_sig_sigint(int signum)
-// {
-//     std::cout << "Receive signum: " << signum << std::endl;
-//     Ros1Runner::stop();
-// }
-Ros1Runner::Ros1Runner() : client_("cognitao_ros", true)
-{
+// namespace cognitao_ros {
+// namespace runner {
+
+
+Ros1Runner::Ros1Runner() : client_("cognitao_ros", true){
+
     // ros::NodeHandle n;
     // ros::Subscriber sub = n.subscribe("stopMe", 1000, stopCallback);
-    stopRequested = false;
-    success = false;
-}
-Ros1Runner::Ros1Runner(const string &action, std::map<std::string, std::string> parameters) : Runner(action, parameters), client_("cognitao_ros", true)
-{
+    if (!ros::isInitialized()) {
+        int n = 0;
+		ros::init(n, nullptr, "cognitao_ros", ros::init_options::AnonymousName);
+	}
+
     stopRequested = false;
     success = false;
 }
 
-bool Ros1Runner::run()
-{
+Ros1Runner::Ros1Runner(const string &action,
+                    std::map<std::string, std::string> parameters):
+                    Runner(action, parameters), client_("cognitao_ros", true){
+
+    if (!ros::isInitialized()) {
+        int n = 0;
+		ros::init(n, nullptr, "cognitao_ros", ros::init_options::AnonymousName);
+	}                    
+    stopRequested = false;
+    success = false;
+}
+
+bool Ros1Runner::run(){
     
     stopRequested = false;
     success = false;
 
-    // signal(SIGINT, h_sig_sigint);
+    
     cout << " DO ACTION _____ " << action_ << endl;
     client_.waitForServer();
 
     actionGoal goalMsg;
     goalMsg.goal.actiontype = action_;
-    for (auto const &x : parameters_)
-    {
+
+    for (auto const &x : parameters_){
+
         cognitao_ros::KeyValue param;
         param.key = x.first;
         param.value = x.second;
+        
         goalMsg.goal.parameters.push_back(param);
     }
 
-    client_.sendGoal(goalMsg, boost::bind(&Ros1Runner::doneCb, this, _1, _2),
+    client_.sendGoal(goalMsg, boost::bind(&Ros1Runner::doneCallback, this, _1, _2),
                      CL::SimpleActiveCallback(),
                      boost::bind(&Ros1Runner::feedbackCb, this, _1));
 
@@ -48,13 +98,13 @@ bool Ros1Runner::run()
     ros::Rate loop_rate(1);
     cognitao_ros::ActionMsgResultConstPtr res = client_.getResult();
 
-    while (true) // check if someone ask to stop the task while the server do the action
-    {
+    while (true){ // check if someone ask to stop the task while the server do the action
+    
         bool goalDone = client_.waitForResult(ros::Duration(1));
 
         if (stopRequested == true) // task end or someone stop the task
         {
-            if (!existRes)
+            if (!resultExist)
             {
                 client_.cancelGoal();
                 ROS_INFO("goal is being canceled");
@@ -70,18 +120,17 @@ bool Ros1Runner::run()
         loop_rate.sleep();
     }
 
-    while (!existRes)
+    while (!resultExist)
     {
     }
     cognitao_ros::ActionMsgResultConstPtr result = client_.getResult();
 
-    if (result->resultvalue == false)
-    {
+    if (result->resultvalue == false){
         cout << "false " << endl;
         return false;
     }
-    else if (result->resultvalue == true)
-    {
+
+    else if (result->resultvalue == true){
         std::cout << "true" << endl;
         success = true;
         return true;
@@ -89,19 +138,27 @@ bool Ros1Runner::run()
 
     return result->resultvalue;
 }
-std::string Ros1Runner::getType() { return "ros1"; };
-void Ros1Runner::stop()
-{
+
+std::string Ros1Runner::getType() { 
+    return "ros1";
+     };
+
+void Ros1Runner::stop(){
+   
     stopRequested = true;
 }
 
-void Ros1Runner::feedbackCb(const actionFeedback &feedback)
-{
+void Ros1Runner::feedbackCb(const actionFeedback &feedback){
+    
     cout << " get feedback " << endl;
 }
-void Ros1Runner::doneCb(const actionlib::SimpleClientGoalState &state, const cognitao_ros::ActionMsgResultConstPtr &result)
-{
+
+void Ros1Runner::doneCallback(const actionlib::SimpleClientGoalState &state, const cognitao_ros::ActionMsgResultConstPtr &result){
+    
     std::cout << "the result is :" << (int)result->resultvalue << std::endl;
-    existRes = true;
+    resultExist = true;
     stopRequested = true; // task completed- stop request to end run() function and return the result
 }
+
+// }
+// }
--- src/ros_dataSource/RosDataSource.cpp
+++ src/ros_dataSource/RosDataSource.cpp
@@ -1,42 +1,77 @@
+/**
+ * @brief ROS 1 cognitao runner 
+ * 
+ * @file RosDataSource.cpp
+ * 
+ * @author Lin Azan (lin@cogniteam.com)
+ * @date 2020-03-15
+ * @copyright Cogniteam (c) 2020
+ *    
+ * MIT License
+ *   
+ * Permission is hereby granted, free of charge, to any person obtaining a  copy
+ * of this software and associated documentation files (the 'Software'), to deal
+ * in the Software without restriction, including without  limitation the rights
+ * to use, copy, modify, merge,  publish,  distribute,  sublicense,  and/or sell
+ * copies of the Software, and  to  permit  persons  to  whom  the  Software  is 
+ * furnished to do so, subject to the following conditions:
+ *   
+ * The   above   copyright   notice   and  this   permission   notice  shall  be
+ * included in all copies or substantial portions of the Software.
+ *   
+ * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY  KIND,  EXPRESS  OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED  TO  THE  WARRANTIES  OF  MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN  NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE  LIABLE  FOR  ANY  CLAIM,  DAMAGES  OR  OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+
 #include <cognitao_ros/ros_dataSource/RosDataSource.h>
 
-RosDataSource::RosDataSource()
-{
-    if (!ros::isInitialized())
-    { cout<<"data source ctor initialization"<<endl;
+RosDataSource::RosDataSource(){
+
+    if (!ros::isInitialized()){ 
         int n = 0;
         ros::init(n, nullptr, "cognitao_ros1");
     }
+
     ros::NodeHandle n_;
-    cout<<"create SUB"<<endl;
     event_sub_ = n_.subscribe("/cognitao_ros/world_model/updates", 1000,
                               &RosDataSource::onDataSourceEvent, this);
-    cout<<"create PUB"<<endl;
+
 
     event_pub_ = n_.advertise<cognitao_ros::EventMsg>("/cognitao_ros/world_model/updates", 1000);
     // serviceGetVar = n_.advertiseService("server_get_var", &RosDataSource::serverGetVar, this);
     // serviceSetVar = n_.advertiseService("server_set_var", &RosDataSource::serverSetVar, this);
+
     spinTHread = std::thread(&RosDataSource::doSpin, this);
+
     spinTHread.detach();
-    cout<<"finish CTOR"<<endl;
 }
 
-RosDataSource::~RosDataSource()
-{
+RosDataSource::~RosDataSource(){
+
     spinTHread.join();
 }
 
-bool RosDataSource::publishUpdateEvent(const string &name, const string &value)
-{
+bool RosDataSource::publishUpdateEvent(const string &name, const string &value){
     cognitao_ros::EventMsg msg;
 
     msg.key = name;
     msg.value = value;
+
     cout << "publish msg " << name << ", " << value << endl;
     event_pub_.publish(msg);
 }
-void RosDataSource::onDataSourceEvent(const cognitao_ros::EventMsg &msg)
-{
-    // WM::setVar(, msg.value);
+void RosDataSource::onDataSourceEvent(const cognitao_ros::EventMsg &msg){
+
     DataSource::variableUpdated(msg.key, msg.value);
+}
+
+void RosDataSource::doSpin() {
+
+    ros::spin();
 }
